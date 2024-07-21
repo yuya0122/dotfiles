9a7a50304baec63664f81e17e2504baddde80833
yuya0122 <yuya0122@gmail.com>
2024-07-21 16:23:35 +0900

    feat: Update macOS setup script

--- macos/setup.sh
+++ macos/setup.sh
@@ -1,27 +1,29 @@
 # --- Finderの設定 ---
 # 隠しファイルを表示
 defaults write com.apple.finder AppleShowAllFiles -boolean true
-
 # すべてのファイル名拡張子を表示（Finder > 環境設定 > 詳細 からも設定可）
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-
 # デスクトップのアイコンを消す
-defaults write com.apple.finder CreateDesktop false && killall Finder
+defaults write com.apple.finder CreateDesktop false
+# リスト形式でファイルを表示する
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
 killall Finder
 
 # --- Dockの設定 ---
 # “自動的に非表示”をオン（Dockを右クリックでも設定可）
 defaults write com.apple.dock autohide -bool true
+# 最近使ったappを表示しない
+defaults write com.apple.dock show-recents -bool false
 
 killall Dock
 
 # --- SystemUIServer関係の設定 ---
 # 時計で日付を表示（例：9月20日(木) 23:00、メニューバー右上の時計からも設定可）
 defaults write com.apple.menuextra.clock DateFormat -string 'EEE MMM d HH:mm'
-
 # スクリーンショットのドロップシャドウを付けない
 defaults write com.apple.screencapture disable-shadow -boolean true
+
 killall SystemUIServer
 
 # ---　トラックパッドの設定　---
