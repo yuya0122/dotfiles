91e94638f991db40f90899b1957cd46027e85ddf
yuya0122 <yuya0122@gmail.com>
2025-01-02 21:34:37 +0900

    add: cursor and chatgpt

---
+++ chatgpt/setup.sh
@@ -0,0 +1,4 @@
+# dockに追加
+dockutil --add "/Applications/ChatGPT.app"
+
+echo 👍 ChatGPT setting is done!
---
+++ cursor/setup.sh
@@ -0,0 +1,26 @@
+# settings.jsonの設置
+ln -sf $PWD/cursor/settings.json ~/Library/Application\ Support/Cursor/User/
+ln -sf $PWD/cursor/keybindings.json ~/Library/Application\ Support/Cursor/User/
+
+# プラグインのインストール
+pkglist=(
+  # GitHub Copilot
+  github.copilot-chat
+  github.copilot
+  # Japanese Language Pack for Visual Studio Code
+  ms-ceintl.vscode-language-pack-ja 
+  # Devcontainer
+  ms-vscode-remote.remote-containers
+  # Git
+  mhutchie.git-graph
+)
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+for i in ${pkglist[@]}; do
+  code --install-extension $i
+done
+
+# dockに追加
+dockutil --add "/Applications/Cursor.app"
+
+echo 👍 Cursor setting is done!
